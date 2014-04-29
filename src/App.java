c0f168092f9b8e40a94f71727207f3030606587e
nikolka1992 <nikolka1992@yahoo.com>
2014-04-28 20:42:55 -0400

    CoupPanel started

--- src/App.java
+++ src/App.java
@@ -3,12 +3,14 @@
 import controller.GameController;
 import model.CoupGameState;
 import view.GameStateView;
+import view.CoupPanel;
 
 public class App {
     public static void main(String ... args) {
         //here is where we start + run the program. Makefile targets this.
         //GameController myControl = GameController.getInstance();
-        GameStateView.main(args);
+        //GameStateView.main(args);
         //CoupGameState();
+    	CoupPanel.main(args);
     }
 }
